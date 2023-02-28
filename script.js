d837d298e1740fc731b02a0ae73b28a37f6431c7
unknown <joelreinoso86@gmail.com>
2023-02-28 19:19:21 -0300

    Archivos para el proyecto final de ticmas

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll('.read-more-btn');
+const textElements = document.querySelectorAll('.hideText');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const textElement = textElements[index];
+    textElement.classList.toggle('showText');
+  });
+});
